050fdee5f89f7e62968622d5ef2e11d62fd60967
tawiahjustin <121239070+tawiahjustin@users.noreply.github.com>
2023-06-23 21:25:45 +0000

    added styles and comment section

--- front_end/src/components/AddQuestions.jsx
+++ front_end/src/components/AddQuestions.jsx
@@ -28,7 +28,21 @@ const AddQuestions = () => {
           <Link to='/home'>
             <div>
               <p>
-                Retour <FontAwesomeIcon icon={faArrowLeft} />
+                Retour{' '}
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-6 h-6'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18'
+                  />
+                </svg>
               </p>
             </div>
           </Link>
@@ -63,7 +77,20 @@ const AddQuestions = () => {
           </div>
           <button className='btn btn-question'>
             Poser ma question
-            <FontAwesomeIcon icon={faArrowRight} />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3'
+              />
+            </svg>
           </button>
         </form>
       </div>
--- front_end/src/views/Comments.jsx
+++ front_end/src/views/Comments.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faArrowLeft,
-  faHeart,
-  faUserCircle,
-} from '@fortawesome/free-solid-svg-icons'
+
 const Comments = () => {
   return (
     <div className='comment__container'>
@@ -16,10 +11,26 @@ const Comments = () => {
             style={{
               display: 'flex',
               flexDirection: 'column',
-              marginRight: '2rem',
+              textAlign: 'center',
+              margin: '0 1rem 3rem 0 ',
+              width: '30px',
             }}
           >
-            <FontAwesomeIcon icon={faHeart} />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
+              />
+            </svg>
+
             <span>10</span>
           </div>
           <h1>wwqx</h1>
@@ -31,7 +42,6 @@ const Comments = () => {
             style={{
               marginTop: '20px',
               marginBottom: '20px',
-              marginRight: '2rem',
             }}
           />
           <div
@@ -41,26 +51,98 @@ const Comments = () => {
             }}
           >
             <h4>2 reponses</h4>
-            <p>
-              <FontAwesomeIcon icon={faUserCircle} />
-              Steph
-            </p>
+            <div style={{ display: 'flex' }}>
+              <div
+                className='svg'
+                style={{
+                  color: 'white',
+                  background: 'lightblue',
+                }}
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 24 24'
+                  fill='currentColor'
+                  className='w-6 h-6 '
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </div>
+              <p>Steph</p>
+            </div>
           </div>
         </div>
         <div>
           <div className='comment__comments'>
             <div
               style={{
-                display: 'flex',
-                flexDirection: 'column',
                 marginRight: '2rem',
               }}
             >
-              <FontAwesomeIcon icon={faHeart} />
+              <div className='svg'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-6 h-6'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
+                  />
+                </svg>
+              </div>
               <span>10</span>
             </div>
             <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+              <div style={{ display: 'flex' }}>
+                <div
+                  className='svg'
+                  style={{
+                    color: 'white',
+                    background: 'lightblue',
+                    marginRight: '10px',
+                  }}
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='currentColor'
+                    className='w-6 h-6'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z'
+                      clipRule='evenodd'
+                    />
+                  </svg>
+                </div>
+                <h4>Justin</h4>
+                <div className='svg'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='w-6 h-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z'
+                    />
+                  </svg>
+                </div>
+                <p>il y'a 1min </p>
+              </div>
               <p>xqqxqx</p>
             </div>
           </div>
@@ -75,9 +157,25 @@ const Comments = () => {
         <button className='btn'>Commenter</button>
       </div>
       <Link to='/home'>
-        <span>
-          Retour <FontAwesomeIcon icon={faArrowLeft} />
-        </span>
+        <div style={{ display: 'flex' }}>
+          <span> Retour</span>
+          <div className='svg'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-1 h-1'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18'
+              />
+            </svg>
+          </div>
+        </div>
       </Link>
     </div>
   )
